32d161ac7bbbc3d56311a82e13f58e4d1f269b28
David Alfageme <dalfagem@mercadona.es>
2022-01-23 13:04:24 +0100

    [Added] invalid letter validation

--- src/app/App.js
+++ src/app/App.js
@@ -1,3 +1,4 @@
+import classNames from "classnames";
 import { useState } from "react";
 import Keyboard from "react-simple-keyboard";
 import "react-simple-keyboard/build/css/index.css";
@@ -6,6 +7,65 @@ import "./App.css";
 function App() {
   const [currentWord, setCurrentWord] = useState("");
   const [currentAttempt, setCurrentAttempt] = useState(0);
+  const [attempts, setAttempts] = useState([]);
+  const correctWord = "sssss";
+
+  const getLetter = (attempt, letterIndex) => {
+    if (currentAttempt === attempt) {
+      return currentWord[letterIndex];
+    }
+
+    return attempts[attempt]?.[letterIndex] || "";
+  };
+
+  const getLetterAlt = (attempt, letterIndex) => {
+    const letter = getLetter(attempt, letterIndex);
+
+    if (attempt >= currentAttempt) {
+      return `Letra ${letterIndex + 1} de palabra ${attempt + 1}: ${
+        letter || "vacía"
+      }`;
+    }
+
+    if (!wordHasLetter(letter)) {
+      return `La letra ${letter} no está en la palabra`;
+    }
+  };
+
+  const getLetterClass = (attempt, letterIndex) => {
+    const letter = getLetter(attempt, letterIndex);
+
+    if (attempt >= currentAttempt) {
+      return;
+    }
+
+    if (!wordHasLetter(letter)) {
+      return "letter--not-present";
+    }
+  };
+
+  const getWordAlt = (attempt) => {
+    if (attempt >= currentAttempt) {
+      return `Palabra ${attempt + 1} vacía`;
+    }
+
+    return `Palabra ${attempt + 1}: ${attempts[attempt]}`;
+  };
+
+  const wordHasLetter = (letter) => {
+    return correctWord.includes(letter);
+  };
+
+  const onKeyPress = (key) => {
+    if (key === "{enter}") {
+      setCurrentAttempt(currentAttempt + 1);
+      attempts.push(currentWord);
+      setCurrentWord("");
+      return;
+    }
+
+    setCurrentWord((word) => word + key);
+  };
 
   return (
     <div className="App">
@@ -15,26 +75,25 @@ function App() {
             className="word"
             key={attempt}
             role="row"
-            aria-label={`Palabra ${attempt + 1} vacía`}
+            aria-label={getWordAlt(attempt)}
           >
             {[0, 1, 2, 3, 4, 5].map((letterIndex) => (
               <div
-                className="letter"
+                className={classNames(
+                  "letter",
+                  getLetterClass(attempt, letterIndex)
+                )}
                 key={letterIndex}
                 role="cell"
-                aria-label={`Letra ${letterIndex + 1} de palabra ${
-                  attempt + 1
-                } vacía`}
+                aria-label={getLetterAlt(attempt, letterIndex)}
               >
-                {currentAttempt === attempt ? currentWord[letterIndex] : ""}
+                {getLetter(attempt, letterIndex)}
               </div>
             ))}
           </div>
         ))}
       </div>
-      <Keyboard
-        onKeyPress={(letter) => setCurrentWord((word) => word + letter)}
-      />
+      <Keyboard onKeyPress={onKeyPress} />
     </div>
   );
 }
--- src/app/App.test.js
+++ src/app/App.test.js
@@ -26,7 +26,7 @@ it("display boxes for each letter", () => {
 
   const row = screen.getByRole("row", { name: "Palabra 1 vacía" });
   const letter = within(row).getByRole("cell", {
-    name: "Letra 1 de palabra 1 vacía",
+    name: "Letra 1 de palabra 1: vacía",
   });
   expect(letter).toBeInTheDocument(6);
 });
@@ -36,12 +36,34 @@ it("keyboard types on grid", async () => {
 
   const row = screen.getByRole("row", { name: "Palabra 1 vacía" });
   const letter = within(row).getByRole("cell", {
-    name: "Letra 1 de palabra 1 vacía",
+    name: "Letra 1 de palabra 1: vacía",
   });
   typeKeyboardLetter("a");
 
   expect(letter).toHaveTextContent("a");
+  expect(letter).toHaveAccessibleName("Letra 1 de palabra 1: a");
 });
+
+it("mark letter in gray if is not present in the correct word", async () => {
+  wrap(App).atPath("/").mount();
+
+  typeKeyboardLetter("a");
+  typeKeyboardLetter("m");
+  typeKeyboardLetter("e");
+  typeKeyboardLetter("n");
+  typeKeyboardLetter("o");
+  enter();
+  const row = screen.getByRole("row", { name: "Palabra 1: ameno" });
+  const letter = within(row).getByRole("cell", {
+    name: "La letra a no está en la palabra",
+  });
+
+  expect(letter).toHaveClass("letter--not-present");
+});
+
+function enter() {
+  typeKeyboardLetter("< enter");
+}
 
 function typeKeyboardLetter(letter) {
   userEvent.click(getKeyboardKey(letter));
