0902dc17d296edd49c0c78ba028e161d2ec25d67
David Alfageme <dalfagem@mercadona.es>
2022-01-23 11:07:08 +0100

    [Added] Wrapito for tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { screen } from "@testing-library/react";
+import { wrap } from "wrapito";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test("renders learn react link", async () => {
+  wrap(App).atPath("/").mount();
+  const linkElement = await screen.findByText(/learn react/i);
+
   expect(linkElement).toBeInTheDocument();
 });
