eb0cff1653f6c3af310839bee0e7478f3c06002f
David Alfageme <dalfagem@mercadona.es>
2022-01-23 11:56:07 +0100

    [Added] Words grid

--- src/app/App.js
+++ src/app/App.js
@@ -5,6 +5,25 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
+      {[0, 1, 2, 3, 4, 5].map((attempt) => (
+        <div
+          className="word"
+          key={attempt}
+          role="row"
+          aria-label={`Palabra ${attempt + 1} vacía`}
+        >
+          {[0, 1, 2, 3, 4, 5].map((letterIndex) => (
+            <div
+              className="letter"
+              key={letterIndex}
+              role="cell"
+              aria-label={`Letra ${letterIndex + 1} de palabra ${
+                attempt + 1
+              } vacía`}
+            ></div>
+          ))}
+        </div>
+      ))}
       <Keyboard />
     </div>
   );
--- src/app/App.test.js
+++ src/app/App.test.js
@@ -1,14 +1,38 @@
-import { screen } from "@testing-library/react";
+import { screen, within } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { wrap } from "wrapito";
 import App from "./App";
 
 it("has a virtual keyboard", async () => {
   wrap(App).atPath("/").mount();
 
-  expect(await screen.findByText("q")).toBeInTheDocument();
-  expect(screen.getByText("w")).toBeInTheDocument();
-  expect(screen.getByText("e")).toBeInTheDocument();
-  expect(screen.getByText("r")).toBeInTheDocument();
-  expect(screen.getByText("t")).toBeInTheDocument();
-  expect(screen.getByText("y")).toBeInTheDocument();
+  expect(getKeyboardKey("w")).toBeInTheDocument();
+  expect(getKeyboardKey("e")).toBeInTheDocument();
+  expect(getKeyboardKey("r")).toBeInTheDocument();
+  expect(getKeyboardKey("t")).toBeInTheDocument();
+  expect(getKeyboardKey("y")).toBeInTheDocument();
 });
+
+it("has a words grid", async () => {
+  wrap(App).atPath("/").mount();
+
+  expect(
+    screen.getByRole("row", { name: "Palabra 1 vacía" })
+  ).toBeInTheDocument();
+});
+
+it("display boxes for each letter", () => {
+  wrap(App).atPath("/").mount();
+
+  const row = screen.getByRole("row", { name: "Palabra 1 vacía" });
+  const letter = within(row).getByRole("cell", {
+    name: "Letra 1 de palabra 1 vacía",
+  });
+  expect(letter).toBeInTheDocument(6);
+});
+
+function getKeyboardKey(letter) {
+  return within(document.querySelector(".react-simple-keyboard")).getByText(
+    letter
+  );
+}
